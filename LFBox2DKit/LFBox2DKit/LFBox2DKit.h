68d9d0c2ca23d1878a21b3dffba1262ded7a5139
wangxiaoxiang <wangxiaoxiang-cn@foxmail.com>
2016-08-15 20:13:37 +0800

    init

---
+++ LFBox2DKit/LFBox2DKit/LFBox2DKit.h
@@ -0,0 +1,51 @@
+//
+//  LFBox2DKit.h
+//  LFBox2DKit
+//
+//  Created by wangxiaoxiang on 16/8/15.
+//  Copyright © 2016年 wangxiaoxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LFBox2DKit.
+FOUNDATION_EXPORT double LFBox2DKitVersionNumber;
+
+//! Project version string for LFBox2DKit.
+FOUNDATION_EXPORT const unsigned char LFBox2DKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LFBox2DKit/PublicHeader.h>
+#import <LFBox2DKit/b2Settings.h>
+#import <LFBox2DKit/b2Draw.h>
+#import <LFBox2DKit/b2Timer.h>
+
+#import <LFBox2DKit/b2CircleShape.h>
+#import <LFBox2DKit/b2EdgeShape.h>
+#import <LFBox2DKit/b2ChainShape.h>
+#import <LFBox2DKit/b2PolygonShape.h>
+
+#import <LFBox2DKit/b2BroadPhase.h>
+#import <LFBox2DKit/b2Distance.h>
+#import <LFBox2DKit/b2DynamicTree.h>
+#import <LFBox2DKit/b2TimeOfImpact.h>
+
+#import <LFBox2DKit/b2Body.h>
+#import <LFBox2DKit/b2Fixture.h>
+#import <LFBox2DKit/b2WorldCallbacks.h>
+#import <LFBox2DKit/b2TimeStep.h>
+#import <LFBox2DKit/b2World.h>
+
+#import <LFBox2DKit/b2Contact.h>
+
+#import <LFBox2DKit/b2DistanceJoint.h>
+#import <LFBox2DKit/b2FrictionJoint.h>
+#import <LFBox2DKit/b2GearJoint.h>
+#import <LFBox2DKit/b2MotorJoint.h>
+#import <LFBox2DKit/b2MouseJoint.h>
+#import <LFBox2DKit/b2PrismaticJoint.h>
+#import <LFBox2DKit/b2PulleyJoint.h>
+#import <LFBox2DKit/b2RevoluteJoint.h>
+#import <LFBox2DKit/b2RopeJoint.h>
+#import <LFBox2DKit/b2WeldJoint.h>
+#import <LFBox2DKit/b2WheelJoint.h>
+
